7ef58d0490f2fe36fcecdcfc8759b63560ac5a50
Surender <surender@example.com>
2026-05-16 10:26:25 +0530

    first draft

--- intex.js
+++ intex.js
@@ -17,6 +17,7 @@ form.addEventListener("submit", function (event) {
         console.error("First Name cannot be empty");
         firstName.nextElementSibling.innerText = "! First Name cannot be empty";
         firstName.nextElementSibling.style.display = "block";
+        firstName.nextElementSibling.style.paddingTop = "3px";
         isValid = false;
     }
 
@@ -27,6 +28,7 @@ form.addEventListener("submit", function (event) {
         dob.nextElementSibling.innerText = "! D.O.B cannot be empty";
         dob.nextElementSibling.style.display = "block";
         isValid = false;
+        dob.nextElementSibling.style.paddingTop = "3px";
     }
 
     const email = document.getElementById("Email");
@@ -35,6 +37,7 @@ form.addEventListener("submit", function (event) {
         email.nextElementSibling.innerText = "! Email cannot be empty";
         email.nextElementSibling.style.display = "block";
         isValid = false;
+        email.nextElementSibling.style.paddingTop = "3px";
     }
 
     const password = document.getElementById("Password");
@@ -43,6 +46,7 @@ form.addEventListener("submit", function (event) {
         password.nextElementSibling.innerText = "! Password cannot be empty";
         password.nextElementSibling.style.display = "block";
         isValid = false;
+        password.nextElementSibling.style.paddingTop = "3px";   
     }
 
     const ConfirmPassword = document.getElementById("ConfirmPassword");
@@ -50,6 +54,7 @@ form.addEventListener("submit", function (event) {
         console.error("Confirm Password cannot be empty");
         ConfirmPassword.nextElementSibling.innerText = "! Confirm Password cannot be empty";
         ConfirmPassword.nextElementSibling.style.display = "block";
+        ConfirmPassword.nextElementSibling.style.paddingTop = "3px";
         isValid = false;
     }
 
@@ -58,6 +63,7 @@ form.addEventListener("submit", function (event) {
         console.error("Address cannot be empty");
         Address.nextElementSibling.innerText = "! Address cannot be empty";
         Address.nextElementSibling.style.display = "block";
+        Address.nextElementSibling.style.paddingTop = "3px";    
         isValid = false;
     }
 
@@ -66,6 +72,7 @@ form.addEventListener("submit", function (event) {
         console.error("Phone Number cannot be empty");
         number.nextElementSibling.innerText = "! Phone Number cannot be empty";
         number.nextElementSibling.style.display = "block";
+        number.nextElementSibling.style.paddingTop = "3px";
         isValid = false;
     }
 
@@ -76,6 +83,7 @@ form.addEventListener("submit", function (event) {
         console.error("Please select gender");
         genderError.innerText = "! Please select gender";
         genderError.style.display = "block";
+        genderError.style.paddingTop = "3px";   
         genderError.style.fontSize = "10px";
         genderError.style.color = "red";
         isValid = false;
@@ -90,6 +98,7 @@ form.addEventListener("submit", function (event) {
         termsError.style.display = "block";
         termsError.style.fontSize = "10px";
         termsError.style.color = "red";
+        termsError.style.paddingTop = "3px";
         isValid = false;
     }
 
@@ -99,73 +108,8 @@ form.addEventListener("submit", function (event) {
 });
 
 
-
-// To Display Form Data as object[Key : Value] in Console
-// document.getElementById("myForm").addEventListener("submit", function(event) {
-//             event.preventDefault();
-
-//             let formData = new FormData(this);
-
-//             for (let [key, value] of formData.entries()) {
-//                 console.log(key + ": " + value);
-//             }
-//             // for htlm page
-//             let displayText = "";
-//             for( let i = 0; i < form.elements.length; i++){
-//                 let element = form.elements[i];
-//                 if (element.type === 'submit' || element.type === 'reset' || element.type === "checkbox" || element.type === "radio") continue;
-
-//                 let value = element.value;
-//                 let id = element.id;
-
-//                 if (id) {
-//                     displayText += id + ": " + value + "\n";
-//                     localStorage.setItem(id, JSON.stringify(value));
-//                 }
-//             }
-
-//             const selectedGender = form.querySelector('input[name="Gender"]:checked');
-//             if (selectedGender) {
-//                 displayText += "Gender: " + selectedGender.value + "\n";
-//                 localStorage.setItem("Gender", JSON.stringify(selectedGender.value));
-//             }
-
-//             document.getElementById("displayData").innerText = displayText;
-
-//             // Commented out reset so later submit listeners can still read the form values
-//             document.getElementById("myForm").reset();
-// });
-
-
-//  //TO Display Form Data as Object[Key : Value] on the HTML Page
-// let result = document.getElementById("result");
-// document.getElementById("myForm").addEventListener("submit",function(event){
-//     event.preventDefault();
-//     let displayText = "";
-//                 for( let i = 0; i < form.elements.length; i++){
-//                     let element = form.elements[i];
-//                     if (element.type === 'submit' || element.type === 'reset' || element.type === "checkbox" || element.type === "radio") continue;
-
-//                     let value = element.value;
-//                     let id = element.id;
-
-//                     if (id) {
-//                         displayText += id + ": " + value + "\n";
-//                     }
-//                 }
-
-//                 const selectedGender = form.querySelector('input[name="Gender"]:checked');
-//                 if (selectedGender) {
-//                     displayText += "Gender: " + selectedGender.value + "\n";
-//                 }
-
-//                 document.getElementById("displayData").innerText = displayText;
-//                 document.getElementById("myForm").reset();
-        
-    
-// });
-
-
+// To Display the data in console 
+// And to display the data on the htm page and store  in the session storage 
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
@@ -194,28 +138,36 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
         user["Gender"] = selectedGender.value;
     }
 
-    // get old data
     let allUsers = JSON.parse(sessionStorage.getItem("users")) || [];
 
-    // add new user
     allUsers.push(user);
 
-    // save again
     sessionStorage.setItem("users", JSON.stringify(allUsers));
+    
+   let displayText = `<table  class = "table">
+        <thead>
+            <tr>
+                <th class = "tablegroup" >Field</th>
+                <th class = "tablegroup" >Value</th>
+            </tr>
+        </thead>
+        <tbody>`;
 
-    // display all submitted users
-    let displayText = "";
-
-    allUsers.forEach(function(data, index) {
-
-        for (let key in data) {
-            displayText += key + ": " + data[key] + "\n";
-        }
-
-        displayText += "----------------------\n";
+        allUsers.forEach(function(data) {
+            for (let key in data) {
+                displayText += `<tr>
+                <td class = "tablegroup" >${key}</td>
+                <td class = "tablegroup" >${data[key]}</td>
+                </tr>`;
+            }displayText += `<tr>
+                <td colspan="2" style="padding: 8px;"></td>
+                </tr>`;
     });
 
-    document.getElementById("displayData").innerText = displayText;
+    displayText += `</tbody></table>`;
+
+    document.getElementById("displayData").innerHTML = displayText;
 
     document.getElementById("myForm").reset();
 });
+
